339c28af97a0005bb09f79801aee25059e2e410b
Abhishek Joshi <abhishekjoshiuo@gmail.com>
2015-10-15 16:42:46 -0500

    time_slot bug fixed

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -2,7 +2,7 @@ class Preference < ActiveRecord::Base
 	belongs_to :day_combination
 	belongs_to :building
 	belongs_to :time_slot
-	has_manys :faculty_preferences, :foreign_key => 'preference1_id', :dependent => :destroy
-	has_manys :faculty_preferences, :foreign_key => 'preference2_id', :dependent => :destroy 
-	has_manys :faculty_preferences, :foreign_key => 'preference3_id', :dependent => :destroy
+	has_many :faculty_preferences, :foreign_key => 'preference1_id', :dependent => :destroy
+	has_many :faculty_preferences, :foreign_key => 'preference2_id', :dependent => :destroy 
+	has_many :faculty_preferences, :foreign_key => 'preference3_id', :dependent => :destroy
 end
