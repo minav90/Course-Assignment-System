dfc1394576d9e37e3498ca2ddf9ad2567a3d4c08
Abhishek Joshi <abhishekjoshiuo@gmail.com>
2015-10-23 00:25:42 -0500

    changes to preference model

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -4,5 +4,5 @@ class Preference < ActiveRecord::Base
 	belongs_to :time_slot
 	has_one :faculty_preference, :foreign_key => 'preference1_id', :dependent => :destroy, :class_name => "FacultyPreference"
 	has_one :other_faculty_preference, :foreign_key => 'preference2_id', :dependent => :destroy, :class_name => "FacultyPreference"
-	has_many :other_faculty_preference, :foreign_key => 'preference3_id', :dependent => :destroy, :class_name => "FacultyPreference"
+	has_one :other_faculty_preference, :foreign_key => 'preference3_id', :dependent => :destroy, :class_name => "FacultyPreference"
 end
