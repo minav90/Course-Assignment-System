d70633a5cae4adfd95fcc610255a91b3abb9ed45
aqib-bhat <aqib.n.bhat@gmail.com>
2015-11-13 15:47:52 -0600

    Adding Cucumber tests for Conflict Checker story

---
+++ features/step_definitions/conflict_checker_steps.rb
@@ -0,0 +1,22 @@
+Given /the following faculty to course mappings exist:/ do |faculty_courses_table|
+    faculty_courses_table.hashes.each do |faculty_course|
+	FacultyCourse.create!(faculty_course)
+    end
+end
+
+Given /the following preferences exist:/ do |preferences_table|
+    preferences_table.hashes.each do |preference|
+	Preference.create!(preference)
+    end
+end
+
+Given /the following faculty_member-preference mappings exist:/ do |faculty_preferences_table|
+    faculty_preferences_table.hashes.each do |faculty_preference|
+	FacultyPreference.create!(faculty_preference)
+    end
+end
+
+Then(/^I should see "(.*?)" and "(.*?)"$/) do |arg1, arg2|
+	page.should have_content(arg1)
+end
+
