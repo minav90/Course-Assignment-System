0a207520412dd326bf08f47ff0f65f5f11433b58
Purvesh Karkamkar <kpurvesh@tamu.edu>
2015-11-13 22:11:06 -0600

    adding rspec tests

--- spec/controllers/conflict_checker_controller_spec.rb
+++ spec/controllers/conflict_checker_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe ConflictCheckerController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+describe ConflictCheckerController do
+	before :each do
+		@day_combinations = [double(:id => '1',:day_combination => 'test')]
+		@time_slots = [double(:id => '1',:time_slot => 'test')]
+		@buildings = [double(:id => '1',:building_name => 'test')]
+	end
+	describe 'getting conflict data' do
+		it 'should call the appropriate model methods to get conflict data' do
+			DayCombination.should_receive(:all).and_return(@day_combinations)
+			TimeSlot.should_receive(:all).and_return(@time_slots)
+			Building.should_receive(:all).and_return(@buildings)
+			post :index
+		end
+		it 'should get the values from session if present' do
+			DayCombination.stub(:all).and_return(@day_combinations)
+                        TimeSlot.stub(:all).and_return(@time_slots)
+                        Building.stub(:all).and_return(@buildings)
+			session[:conflicts] = []
+			session[:dayComboId] = []
+			session[:timeSlotId] = []
+			session[:buildingId] = []
+			session[:computed] = true
+			post :index
+		end
+	end
 end
