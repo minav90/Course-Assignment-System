f5c4c6ecb8703ba9b50e0cabb17f5e9887a9c53a
sibi <rsibi@tamu.edu>
2015-11-29 14:17:20 -0600

    fpage

---
+++ app/controllers/faculty_preferences_controller.rb
@@ -0,0 +1,42 @@
+class FacultyPreferencesController < ApplicationController
+  def index
+  end
+
+  def new
+    #@faculty_course = FacultyCourse.find(params[:faculty_course_id])
+    @faculty_preference = FacultyPreference.new(faculty_course: @faculty_course) 
+    @faculty_preference.build_preference1
+    @faculty_preference.build_preference2
+    @faculty_preference.build_preference3
+  	@building_options = Building.all
+    @time_slot_options = TimeSlot.all
+    @classroomTiming = ClassroomTiming.where(:semester_id => session[:semester_id]).all
+    @day_options = DayCombination.all
+    @faculty_courses = FacultyCourse.all
+  end
+
+  def create
+    @faculty_preference = FacultyPreference.new(faculty_preference_params)
+    if @faculty_preference.save
+      redirect_to @faculty_preference
+    else
+      render :new  
+    end
+  end
+
+  def show
+    @faculty_preference = FacultyPreference.find(params[:id])
+  end 
+
+  private
+  def faculty_preference_params
+    params.require(:faculty_preference).permit(:faculty_course_id, :semester_id, preference1: preference_params, preference2: preference_params, preference3: preference_params)
+  end
+
+ 
+
+  def preference_params
+    [:day_combination_id, :building_id, :time_slot_id]
+  end
+end
+
