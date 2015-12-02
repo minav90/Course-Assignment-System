f26e28b4a26cbddef754ed76b824e25eabdf5bc3
ogsmin_123 <ogsmin_123@tamu.edu>
2015-12-01 20:19:13 -0600

    capacity text box numeric only

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function () {
+  //called when key is pressed in textbox
+  $("#classroomcapacity").keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        //display error message
+        $("#capacityerrmsg").html("Digits Only").show().fadeOut("slow");
+               return false;
+    }
+   });
+});
