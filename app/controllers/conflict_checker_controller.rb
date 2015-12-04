00e497c7dd0798b4f8053059af4a612d5d0f54d6
aqib-bhat <aqib.n.bhat@gmail.com>
2015-12-04 07:37:54 -0600

    Refactored the CC Controller and added comments for the CC Controller and Helper class methods

--- app/controllers/conflict_checker_controller.rb
+++ app/controllers/conflict_checker_controller.rb
@@ -1,26 +1,26 @@
+# @author: Aqib Niaz Bhat Abhishek Vinod Kumar Joshi
 # Controller class for the Conflict Checker page
 class ConflictCheckerController < ApplicationController
 # Inlcuding the Helper class for Conflict Checker
 include ConflictCheckerHelper
 
+  # Handles request to display the landing page for the feature
   def index
-    
+
 	if ((session[:semester_id] == nil) || (session[:semester_id] == ""))
 		flash[:error] = "Please choose a semester"
 		redirect_to root_path
 	else
 	    @dayCombos = DayCombination.all
 	    
-	    @timeSlots = TimeSlot.all
-	    
-	    @buildings_table_values = Building.all
+	    buildings_table_values = Building.all
 	    
 	    @buildings = Array.new
 	    
 	    @buildings.insert(0, "")
 	    
 	    i = 1
-	    @buildings_table_values.each do |b|
+	    buildings_table_values.each do |b|
 	    	@buildings.insert(i, [b.id, b.building_name])
 	    	i+=1
 	    end 
@@ -48,124 +48,135 @@ include ConflictCheckerHelper
 
   end
   
+  # Creates the objects whose data needs to be displayed on the page
   def create  	
   	
-    @day_combo_row = findDayCombinationDataFromId(params["conflict_checker"]["day_combinations_id"]) 
-    @relevantTSlots = getTimeSlotsForDayComboAndRange(params["conflict_checker"]["day_combinations_id"], params["conflict_checker"]["time_ranges"])
-    @building_row = findBuildingDataFromId(params["conflict_checker"]["buildings_id"])
+    day_combo_row = findDayCombinationDataFromId(params["conflict_checker"]["day_combinations_id"])
+    # Call the method to get the time slots based on the day combination and range chosen by the user
+    relevantTSlots = getTimeSlotsForDayComboAndRange(params["conflict_checker"]["day_combinations_id"], params["conflict_checker"]["time_ranges"])
+    building_row = findBuildingDataFromId(params["conflict_checker"]["buildings_id"])
     
-    @building = params["conflict_checker"]["buildings_id"]
+    building = params["conflict_checker"]["buildings_id"]
     
-    
-    session[:dayComboId] = params["conflict_checker"]["day_combinations_id"]
-    session[:timeRanges] = params["conflict_checker"]["time_ranges"]
-    session[:buildingId] = params["conflict_checker"]["buildings_id"]
+    # Call the method to store the user inputs in the session
+    setInputsInSession()
 
     @semester_id = session[:semester_id]
 
-    @relevant_preferences = Array.new # Course Name, Course Title, Faculty name, preference, preference #
-    i = j = 0
+    relevant_preferences = Array.new # Course Name, Course Title, Faculty name, preference, preference #
+    i = 0
     
     @conflicts = Array.new # Array to store conflict data: Faculty Name, Course Name, Course Title, Time-Slot, Building name, Note, Preference #, Assigned?
     
-    @faculty_preferences = FacultyPreference.where("semester_id = ?",@semester_id)
+    faculty_preferences = FacultyPreference.where("semester_id = ?",@semester_id)
     
-    @relevantTSlots.each do |relevantTSlot|
-    	@time_slot_row = findTimeSlotDataFromId(relevantTSlot)
-    	@faculty_preferences.each do |faculty_preference|
+    relevantTSlots.each do |relevantTSlot|
+    	time_slot_row = findTimeSlotDataFromId(relevantTSlot)
+    	faculty_preferences.each do |faculty_preference|
 
-        	@pref1_id = faculty_preference.preference1_id
-        	@pref_1 = Preference.find_by id: @pref1_id
-        	@pref2_id = faculty_preference.preference2_id
-        	@pref_2 = Preference.find_by id: @pref2_id
-        	@pref3_id = faculty_preference.preference3_id
-        	@pref_3 = Preference.find_by id: @pref3_id
+        	pref1_id = faculty_preference.preference1_id
+        	pref_1 = Preference.find_by id: pref1_id
+        	pref2_id = faculty_preference.preference2_id
+        	pref_2 = Preference.find_by id: pref2_id
+        	pref3_id = faculty_preference.preference3_id
+        	pref_3 = Preference.find_by id: pref3_id
 
-		@course_ids = findCoursesForFaculty(faculty_preference.faculty_course_id)
-		@course_ids.each do |courseID|
-		  @courseRow = courseDetails(courseID)
-		  @course_name = @courseRow.course_name
-		  @course_title = @courseRow.CourseTitle	 
+		course_ids = findCoursesForFaculty(faculty_preference.faculty_course_id)
+		course_ids.each do |courseID|
+
+		  courseRow = courseDetails(courseID)
+		  course_name = courseRow.course_name
+		  course_title = courseRow.CourseTitle	 
 		 
-		  @faculty_id = findFacultyforCourse(courseID)
-		  @faculty_name = findFacultyName(@faculty_id)
-		   # Check Pref 1
-		   if ( (@pref_1) && (@day_combo_row) && (@time_slot_row) &&
-		 	  (@pref_1.day_combination_id == @day_combo_row.id) && ((findTimeSlotDataFromId((@pref_1.time_slot_id)).time_slot == @time_slot_row.time_slot)) && checkBuildingOnInput(@building, @pref_1, @building_row))
+		  faculty_id = findFacultyforCourse(courseID)
+		  faculty_name = findFacultyName(faculty_id)
+		  # Check Pref 1
+		  if ( (pref_1) && (day_combo_row) && (time_slot_row) &&
+		 	  (pref_1.day_combination_id == day_combo_row.id) && ((findTimeSlotDataFromId((pref_1.time_slot_id)).time_slot == time_slot_row.time_slot)) && checkBuildingOnInput(building, pref_1, building_row))
 		 
-			@relevant_preferences.insert(i, [@course_name, @course_title, @faculty_name, @pref_1, 1])
-			i += 1
+		    relevant_preferences.insert(i, [course_name, course_title, faculty_name, pref_1, 1])
+		    i += 1
 				
-		   end
+		  end
 		 
-		       # Check Pref 2
-		 if ( (@pref_2) && (@day_combo_row) && (@time_slot_row) && 
-		 	(@pref_2.day_combination_id == @day_combo_row.id) && ((findTimeSlotDataFromId((@pref_2.time_slot_id)).time_slot == @time_slot_row.time_slot)) && checkBuildingOnInput(@building, @pref_2, @building_row))	
+		  # Check Pref 2
+		  if ( (pref_2) && (day_combo_row) && (time_slot_row) && 
+		 	(pref_2.day_combination_id == day_combo_row.id) && ((findTimeSlotDataFromId((pref_2.time_slot_id)).time_slot == time_slot_row.time_slot)) && checkBuildingOnInput(building, pref_2, building_row))	
 		 
-			@relevant_preferences.insert(i, Array[@course_name, @course_title, @faculty_name, @pref_2, 2])
-			i += 1
+		    relevant_preferences.insert(i, Array[course_name, course_title, faculty_name, pref_2, 2])
+		    i += 1
 				
-		 end
+		  end
 		 
-		     # Check Pref 3
-		 if ( (@pref_3) && (@day_combo_row) && (@time_slot_row) && 
-		 	(@pref_3.day_combination_id == @day_combo_row.id) && ((findTimeSlotDataFromId((@pref_3.time_slot_id)).time_slot == @time_slot_row.time_slot)) && checkBuildingOnInput(@building, @pref_3, @building_row))
+		  # Check Pref 3
+		  if ( (pref_3) && (day_combo_row) && (time_slot_row) && 
+		 	(pref_3.day_combination_id == day_combo_row.id) && ((findTimeSlotDataFromId((pref_3.time_slot_id)).time_slot == time_slot_row.time_slot)) && checkBuildingOnInput(building, pref_3, building_row))
 		 
-			@relevant_preferences.insert(i, Array[@course_name, @course_title, @faculty_name, @pref_3, 3])
-			i += 1
+		    relevant_preferences.insert(i, Array[course_name, course_title, faculty_name, pref_3, 3])
+		    i += 1
 				
-		 end
+		  end
+
 		end
 		 
-	end
-     end
+    	end
+    end
 
-	@relevant_preferences.each do |relevant_preference|
-	
-		        # Faculty Name
-			@fac_name = relevant_preference[2]
-			# Course Details
-			@cour_name = relevant_preference[0] # .course_name
-			@cour_title = relevant_preference[1] # .CourseTitle
-			# Day-Combo
-			@day_combo = (findDayCombinationDataFromId((relevant_preference[3]).day_combination_id)).day_combination
-			# Time-Slot
-			@time_slot = (findTimeSlotDataFromId((relevant_preference[3]).time_slot_id)).time_slot
-			# Building
-			res = findBuildingDataFromId((relevant_preference[3]).building_id)
-			if res != ""
-				@building_name = (res).building_name
-			else
-				@building_name = ""
-			end
-			# Note
-			@note = relevant_preference[3].note
-			# Preference number
-			@pref_no = relevant_preference[4]
-			# Variable to check course has been assigned to a slotAlready Assigned
-			@already_asgn = isAssigned((relevant_preference[3]).building_id, (relevant_preference[3]).day_combination_id, (relevant_preference[3]).time_slot_id, @cour_name, @fac_name) 
-		    @conflicts.insert(j, Array[@fac_name, @cour_name, @cour_title, @day_combo, @time_slot, @building_name, @note, @pref_no, @already_asgn])
-			j += 1
-	end
+    # Call the method to get the conflicts from the relevant preferences
+    @conflicts = getConflictsFromRelevantPreferences(relevant_preferences)
     
     session[:computed] = true
     session[:conflicts] = @conflicts
   	redirect_to conflict_checker_index_path
   end
+
+  # This method stores the user inputs in the session
+  def setInputsInSession()
+    session[:dayComboId] = params["conflict_checker"]["day_combinations_id"]
+    session[:timeRanges] = params["conflict_checker"]["time_ranges"]
+    session[:buildingId] = params["conflict_checker"]["buildings_id"]
+  end
+
+  # This method gets the conflicts from relevant preferences
+  # @param relevant_preferences The relevant (based on the search inputs) preferences gathered in create
+  def getConflictsFromRelevantPreferences(relevant_preferences)
+    conflicts = Array.new
+    j = 0
+
+    relevant_preferences.each do |relevant_preference|
+
+      # Faculty Name
+      fac_name = relevant_preference[2]
+      # Course Details
+      cour_name = relevant_preference[0] # .course_name
+      cour_title = relevant_preference[1] # .CourseTitle
+      # Day-Combo
+      day_combo = (findDayCombinationDataFromId((relevant_preference[3]).day_combination_id)).day_combination
+      # Time-Slot
+      time_slot = (findTimeSlotDataFromId((relevant_preference[3]).time_slot_id)).time_slot
+      # Building
+      res = findBuildingDataFromId((relevant_preference[3]).building_id)
+      if res != ""
+        building_name = (res).building_name
+      else
+        building_name = ""
+      end
+      # Note
+      note = relevant_preference[3].note
+      # Preference number
+      pref_no = relevant_preference[4]
+      # Variable to check course has been assigned to a slotAlready Assigned
+      already_asgn = isAssigned((relevant_preference[3]).building_id, (relevant_preference[3]).day_combination_id, (relevant_preference[3]).time_slot_id, cour_name, fac_name)
+
+      conflicts.insert(j, Array[fac_name, cour_name, cour_title, day_combo, time_slot, building_name, note, pref_no, already_asgn])
+      j += 1
+
+    end
+
+    return conflicts
+  # end of the method
+  end
+
+# end of the class
 end
-
-=begin
-Models and their columns:
-1. Building: building_id, building_name (s)
-2. Room: room_id, room_name (s), building_id (i), Capacity (i)
-3. DayCombination: day_combination_id, day_combination (s)
-4. TimeSlot: time_slot_id, time_slot (s)
-5. Preference: preference_id, note, building_id, day_combination_id, time_slot_id, semester_id (i)
-6. FacultyPreference: faculty_preference_id, faculty_course_id (i), preference1_id (i), preference2_id (i), preference3_id (i), semester_id (i)
-7. Course: course_id (i), course_name (s), CourseTitle (s)
-8. FacultyCourse: faculty_id (i), course1_id (i), course2_id (i), course3_id (i), semester_id (i)
-9. Faculty: faculty_id (i), faculty_name (s)
-10. CourseAssignment: faculty_id (i), course_id (i), room_id (i), day_combination_id (i), time_slot_id (i), semester_id (i)
-=end
-
 
--- app/helpers/conflict_checker_helper.rb
+++ app/helpers/conflict_checker_helper.rb
@@ -1,4 +1,9 @@
+# @author: Abhishek Vinod Kumar Joshi
+# Helper class for the Conflict Checker page
 module ConflictCheckerHelper
+	
+	# This method is used to get the course details from the DB using the course id
+	# @param courseId The id of the course
 	def courseDetails(courseId)
 		courseTable = Course.all
 		courseId = courseId.to_i
@@ -9,17 +14,21 @@ module ConflictCheckerHelper
 		end
 	end
 	
+	# This method is used to get the faculty details from the DB using the course id
+	# @param courseId The id of the course
 	def findFacultyforCourse(courseId)
 		facultycourseTable = FacultyCourse.where("semester_id = ?",@semester_id)
 		courseId = courseId.to_i
 		facultycourseTable.each do |facultycourserow|
 			if ((facultycourserow.course1_id == courseId) || (facultycourserow.course2_id == courseId))
-				return facultycourserow.faculty_id # @facultyid = 
+				return facultycourserow.faculty_id
 			end
 
 		end
 	end
 	
+	# This method is used to get the ids of the courses that a faculty is teaching using the faculty-course matching id
+	# @param faculty_course_id The faculty-course matching id
 	def findCoursesForFaculty(faculty_course_id)
 		returnArray = Array.new
 		i = 0
@@ -40,16 +49,20 @@ module ConflictCheckerHelper
 		end
 	end
 
+	# This method is used to get the name of the faculty using the faculty id
+	# @param facultyId The faculty id
 	def findFacultyName(facultyId)
 		facultyTable = Faculty.all
 		facultyId = facultyId.to_i
 		facultyTable.each do |facultyrow|
 			if facultyrow.id == facultyId
-				return facultyrow.faculty_name # @facultyName = 
+				return facultyrow.faculty_name
 			end
 		end
 	end
 	
+	# This method is used to the details of a building using the building id
+	# @param buildingId The building id
 	def findBuildingDataFromId(buildingId)
 		buildingTable = Building.all
 		if buildingId != ""
@@ -65,6 +78,8 @@ module ConflictCheckerHelper
 		end
 	end
 	
+	# This method is used to get the details of a day combination using the day combination id
+	# @param dayComboId The day combination id
 	def findDayCombinationDataFromId(dayComboId)
 		daycomboTable = DayCombination.all
 		dayComboId = dayComboId.to_i
@@ -75,6 +90,8 @@ module ConflictCheckerHelper
 		end
 	end
 	
+	# This method is used to find the details of the time slot using the time slot id
+	# @param timeSlotId The time slot id
 	def findTimeSlotDataFromId(timeSlotId)
 		timeslotTable = TimeSlot.all
 		timeSlotId = timeSlotId.to_i
@@ -85,6 +102,12 @@ module ConflictCheckerHelper
 		end
 	end
 	
+	# This method returns "Yes" if the assignment has already been done for the given details
+	# @param buildingId The building id
+	# @param dayComboId The day combination id
+	# @param timeslotId The time slot id
+	# @param courseName The course name
+	# @param facultyName The faculty name
 	def isAssigned(buildingId, dayComboId, timeslotId, courseName, facultyName)
 		if CourseAssignment.count > 0
 			@semester_id = session[:semester_id]
@@ -106,22 +129,26 @@ module ConflictCheckerHelper
 		return "No"
 	end
 	
-	# New method for range
+	# This method is used to get the time slot ids for a given time range and day combination
+	# @param dayComboId The day combination id
+	# @param timeRange The time range
 	def getTimeSlotsForDayComboAndRange(dayComboId, timeRange)
-		@timeranges = TimeRange.all
+		timeranges = TimeRange.all
 		dayComboId = dayComboId.to_i
-		@timeranges.each do |tr|
+		timeranges.each do |tr|
 			if (tr.day_combination_id == dayComboId && tr.t_range == timeRange)
 				return (tr.t_slots).split(',')
 			end
 		end
 	end
 
+	# This method is used to get the building id for a given room id
+	# @param roomId The room id
 	def getBuildingIdfromRoom(roomId)
 	
-		@roomTable = Room.all
+		roomTable = Room.all
 		roomId = roomId.to_i
-		@roomTable.each do |r|
+		roomTable.each do |r|
 			
 			if r.id == roomId
 				return r.building_id
@@ -129,6 +156,10 @@ module ConflictCheckerHelper
 		end
 	end
 	
+	# This method returns true if the building entered by the user matches with that of a preference or was not enetered at all
+	# @param buildingInput The building chosen by the user as an input on the Conflict Checker page
+	# @param preference A given preference for which we need to check
+	# @param buildingRowData A building's data for which we need to check
 	def checkBuildingOnInput(buildingInput, preference, buildingRowData)
 		if buildingInput ==""
 			return true
@@ -141,6 +172,7 @@ module ConflictCheckerHelper
 		end
 	
 	end
-		
+
+# End of the class		
 end
 
